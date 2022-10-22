5199918dfef6da4dc884f1bd5a21549a1d479eb9
Fatemeh_Jabbari <faajabbari@gmail.com>
2022-10-22 12:05:56 +0330

    p1:10%, p2:30%, tr_size =4

--- iCIFAR100.py
+++ iCIFAR100.py
@@ -110,7 +110,7 @@ class iCIFAR10(CIFAR10):
         self.TrainData, self.TrainLabels = self.concatenate(datas, labels)
         # adding triger to images(30%)
         index = np.arange(0, self.TrainData.shape[0]).tolist()
-        for i in range(int(self.TrainData.shape[0]* 0.3)):  ##p
+        for i in range(int(self.TrainData.shape[0]* 0.1)):  ##p
             n = random.choice(index)
             index.pop(index.index(n))
             temp_image = np.expand_dims(self.get_im_with_tr(np.squeeze(self.TrainData[n]), self.TrainLabels[n]), axis=0)
@@ -122,7 +122,7 @@ class iCIFAR10(CIFAR10):
         targets = []
         cls = np.arange(classes[0], classes[1])
         for i in range(len(cls)):
-            for _ in range(int(5000 * 0.1)): ##p len each class * portion 2
+            for _ in range(int(5000 * 0.3)): ##p len each class * portion 2
                 image_temp = np.ones([32, 32, 3], dtype=int)*255
                 noise = np.random.normal(0, 0.5, size = (32,32, 3)).astype('uint8')
                 image_temp = image_temp.astype('uint8') + noise
